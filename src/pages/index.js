448a03de4a3413e8c9f29c4ace57933171afb6fc
skyappgt <itsielec@gmail.com>
2020-01-20 22:22:00 -0600

    hrefstyle

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,12 @@ import {
 } from '../components';
 import resume from '../../data/profile';
 import '../main.css';
+import hrefstyle from '../assets/style.css';
 
 const Home = () => (
   <main className="antialiased text-neutral-900 bg-neutral-100 min-h-screen sm:p-5">
     <SEO title="Resume" />
-    <link rel="stylesheet" type="text/css" href="/style.css" />
+    <link rel="stylesheet" type="text/css" src={hrefstyle} />
     <div className="container mx-auto shadow bg-white py-5 px-10">
       <Header
         contacts={resume.contact}
